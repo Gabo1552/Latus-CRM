799a37e5440712393bafe8ae5e7f6cf2e56edbf4
Gabo1552 <gaby.herrera@hotmail.es>
2026-06-07 17:47:18 -0300

    login5

--- backend/server.py
+++ backend/server.py
@@ -3058,6 +3058,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_methods=["*"],
     allow_headers=["*"],
 )
